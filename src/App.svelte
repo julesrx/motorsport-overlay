6866c3cf7085db3d13f566ff0cb6725d72fdc499
julesrx <25800503+julesrx@users.noreply.github.com>
2022-07-02 17:59:41 +0200

    sort

--- src/App.svelte
+++ src/App.svelte
@@ -14,27 +14,11 @@
         position: i
       };
     });
-
-  // setInterval(() => {
-  //   drivers = [
-  //     ...drivers,
-  //     {
-  //       id: crypto.randomUUID(),
-  //       name: 'Radnom',
-  //       position: new Date().getSeconds()
-  //     }
-  //   ];
-  // }, 1000);
-
-  $: orderedDrivers = [...drivers.sort((a, b) => a.position - b.position)];
-  $: {
-    console.log(orderedDrivers);
-  }
 </script>
 
 <main>
   <div>
-    {#each orderedDrivers as driver}
+    {#each drivers.sort((a, b) => a.position - b.position) as driver}
       <div on:click={() => driver.position++}>
         {JSON.stringify(driver)}
       </div>
