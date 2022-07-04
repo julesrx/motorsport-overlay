9add3ca396519864c220989a1ad698ec5d69fa6d
julesrx <25800503+julesrx@users.noreply.github.com>
2022-07-05 01:52:07 +0200

    up or down

--- src/components/DriverCard.vue
+++ src/components/DriverCard.vue
@@ -10,10 +10,13 @@ const emit = defineEmits<{
 const props = defineProps<{ driver: Driver; index: number }>();
 
 const showArrow = ref(false);
+const positionGained = ref(false);
 watch(
   () => props.index,
   (newIndex, oldIndex) => {
     if (newIndex === oldIndex) return;
+
+    positionGained.value = newIndex < oldIndex;
 
     showArrow.value = true;
     setTimeout(() => {
@@ -33,17 +36,21 @@ const cardHeight = 2;
     @click.right.prevent="() => emit('update-position', false)"
   >
     {{ driver.position }}
+
+    <div class="arrow" :style="{ visibility: showArrow ? 'visible' : 'hidden' }">
+      {{ positionGained ? '^' : 'V' }}
+    </div>
   </div>
 </template>
 
 <style scoped>
 .driver-card {
-  width: 2em;
+  width: 4em;
   position: absolute;
-  transition: all 0.5s ease 0s;
+  transition: all 0.5s;
   user-select: none;
   display: flex;
-  justify-content: center;
+  padding-left: 0.5em;
   align-items: center;
   background-color: #d4ff00;
   border: 1px solid #000;
@@ -53,5 +60,11 @@ const cardHeight = 2;
 
 .driver-card.updated {
   background-color: red;
+}
+
+.driver-card .arrow {
+  position: absolute;
+  right: 0.5em;
+  transition: visibility 0s, opacity 0.5s linear;
 }
 </style>
